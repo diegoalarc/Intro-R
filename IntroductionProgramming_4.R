931c3c004c88b160fa74ec165027b558b01b8b71
mw-95 <witt.marius@web.de>
2019-11-12 16:51:25 +0100

    Message

--- IntroductionProgramming_4.R
+++ IntroductionProgramming_4.R
@@ -4,6 +4,7 @@
 
 install.packages("brickr") # not available for r 3.6.1
 install.packages("rwhatsapp") # analyse you r conversations
+install.packages("ggthemes")
 
 setwd("F:/Eagle/Introduction_Programming/Scripts")
 df <- read.csv("output.csv")
@@ -15,6 +16,7 @@ library(rayshader)
 library(gganimate)
 library(gifski)
 library(transformr)
+library(ggthemes)
 
 x11()
 x <-  data.frame(x=1, y=1, label ="ggplot2 introduction \n@ EAGLE")
@@ -50,17 +52,17 @@ ggplot(df, aes(d, fill = c)) +
 
 # # 3d plotting
 # 
-# mtplot = ggplot(mtcars) + 
-#   geom_point(aes(x = mpg, y = disp, color = cyl)) + 
-#   scale_color_continuous(limits = c(0, 8))
-# 
-# par(mfrow = c(1, 2))
-# plot_gg(mtplot, width = 3.5, raytrace = FALSE, preview = TRUE)
-# 
-# plot_gg(mtplot, width = 3.5, multicore = TRUE, windowsize = c(800, 800), 
-#         zoom = 0.85, phi = 35, theta = 30, sunangle = 225, soliddepth = 100)
-# 
-# 
+mtplot = ggplot(mtcars) +
+  geom_point(aes(x = mpg, y = disp, color = cyl)) +
+  scale_color_continuous(limits = c(0, 8))
+
+par(mfrow = c(1, 2))
+plot_gg(mtplot, width = 3.5, raytrace = FALSE, preview = TRUE)
+
+plot_gg(mtplot, width = 3.5, multicore = TRUE, windowsize = c(800, 800),
+        zoom = 0.85, phi = 35, theta = 30, sunangle = 225, soliddepth = 100)
+
+
 # normal plotting continues
 
 ggplot(df, aes(d,a )) + geom_boxplot() + geom_jitter(alpha = .5, width = .3, color = "blue")
@@ -105,6 +107,8 @@ c + geom_violin() +geom_jitter()
 # density plot
 ggplot(df,aes(x=TimeScan))
 
+
+
 # ridgeplots
 ggplot(df, aes(x=MOD.evi, y=LCname)) + geom_density_ridges2()
 
@@ -113,17 +117,34 @@ ggplot(df, aes(x=MOD.evi, y=LCname)) + geom_density_ridges2()
 ## Animating in R
 # playing around with plot animations
 
-p <- ggplot(data=df, aes(y=L8.ndvi, x=L8.evi, color=LCname, size=SRTM))+
-  geom_point(alpha=.8)
+p <- ggplot(data=df, aes(y=L8.ndvi, x=L8.evi, color=LCname, ))+
+  geom_point(alpha=.8) + geom_smooth(method = lm)
 
 d <- ggplot(data=df, aes(L8.savi, color=LCname, fill = LCname)) + geom_density(alpha = 0.5) 
   
-d + transition_states(LCname, transition_length = 20, state_length = 20, wrap = TRUE) +
+p + transition_states(LCname, transition_length = 20, state_length = 20, wrap = TRUE) +
   ease_aes('sine-in-out')+
   # labs(title = "semester:{closest_state}")+
   enter_fade() +
-  exit_fade()
-  
+  exit_shrink() + 
+  theme_minimal()
 
 
-head(df)
+# Add new column to df and cut SRTM
+
+df$SRTM_Rec <- cut(df$SRTM,
+                   breaks = c(-Inf, 300, 400, Inf),
+                   labels = c("<300m", "300-400m", ">400m"))
+
+# pointplot wrapped around SRTM_Rec
+
+p <- ggplot(data=df, aes(y=L8.ndvi, x=L8.evi, color=LCname, ))+
+  geom_point(alpha=.8) + geom_smooth(method = lm) +
+  facet_wrap(~SRTM_Rec)
+
+p + transition_states(LCname, transition_length = 20, state_length = 20, wrap = TRUE) +
+  ease_aes('sine-in-out')+
+  # labs(title = "semester:{closest_state}")+
+  enter_fade() +
+  exit_shrink() + 
+  theme_tufte()
